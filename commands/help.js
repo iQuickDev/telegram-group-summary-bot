466356838284a7518b084c645a70175d2926b09d
iQuick <iquickpublic@gmail.com>
2025-11-06 18:30:56 +0100

    remove command "custom"

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ module.exports = async (ctx, db) => {
         "/view <id> - Mostra un riassunto specifico per ID\n" +
         "/last - Mostra l'ultimo riassunto generato\n" +
         "/self - Genera una descrizione di te basata sui tuoi messaggi\n" +
-        "/custom <messaggio> - Chiedi qualcosa di specifico sui tuoi messaggi\n" +
         "/prompt -c [all|self|@user1] -l [limit] -m [messaggio] - Prompt personalizzato con parametri\n" +
         "/help - Mostra questo messaggio di aiuto\n" +
         "Powered by iQuick"
